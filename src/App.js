20c8080abf0a6373f437780592d0db80698b3f3d
chrisbrake <chris.brake@gmail.com>
2020-12-20 14:08:26 -0700

    simplified

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { Switch } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 
 const App = (props) => {
-  let [checked, setChecked] = React.useState(true);
 
-  const toggleChecked = () => {
-    window.console.log(`Setting checked to ${!checked}`)
-    setChecked(!checked)
-  };
+  let [checked, setChecked] = React.useState(true);
 
   return (
     <ThemeProvider >
-      <Switch color="primary" checked={checked} onChange={toggleChecked} />
+      <Switch color="primary" checked={checked} onChange={() => setChecked(!checked)} />
     </ThemeProvider>
   );
 }
