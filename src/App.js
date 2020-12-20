c0bac519c83eb6fcc42196502c5d21eee1d3e8bb
chrisbrake <chris.brake@gmail.com>
2020-12-20 14:14:03 -0700

    Theme fully implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Switch } from '@material-ui/core';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { colors, Switch } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 const App = (props) => {
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: colors.purple[500],
+      },
+      secondary: {
+        main: colors.green[500],
+      },
+    },
+  });
 
   let [checked, setChecked] = React.useState(true);
 
   return (
-    <ThemeProvider >
+    <ThemeProvider theme={theme}>
       <Switch color="primary" checked={checked} onChange={() => setChecked(!checked)} />
     </ThemeProvider>
   );
