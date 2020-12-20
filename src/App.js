13ba51428eb8f35691cd0a5636ae0d15e3adbacf
chrisbrake <chris.brake@gmail.com>
2020-12-19 19:51:33 -0700

    Theme priovider set up... now I need a theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Switch } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 
-function App() {
+const App = (props) => {
   let [checked, setChecked] = React.useState(true);
 
   const toggleChecked = () => {
     window.console.log(`Setting checked to ${!checked}`)
     setChecked(!checked)
   };
-  
-  return <Switch color="primary" checked={checked} onChange={toggleChecked} />;
+
+  return (
+    <ThemeProvider >
+      <Switch color="primary" checked={checked} onChange={toggleChecked} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
