3a9b483e2951786ece01fea833db6a7538e32728
chrisbrake <chris.brake@gmail.com>
2020-12-20 21:19:00 -0700

    added nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Switch, Button, CssBaseline, TextField } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+
+import IconButton from '@material-ui/core/IconButton';
+import AlarmIcon from '@material-ui/icons/Alarm';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
 const App = (props) => {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
@@ -16,7 +32,7 @@ const App = (props) => {
   }),
     [prefersDarkMode],
   );
-
+  const [value, setValue] = React.useState(0);
   let [primaryChecked, setPrimaryChecked] = React.useState(true);
   let [secondaryChecked, setSecondaryChecked] = React.useState(true);
 
@@ -34,8 +50,79 @@ const App = (props) => {
       <Switch color="secondary" checked={secondaryChecked} onChange={() => setSecondaryChecked(!secondaryChecked)} />
       <Switch disabled checked={secondaryChecked} onChange={() => setSecondaryChecked(!secondaryChecked)} />
 
+      <br />
+      <br />
+      <br />
 
-      <Button variant="contained">Default</Button>
+      <div>
+        <Button
+          variant="contained"
+          color="secondary"
+          startIcon={<DeleteIcon />}
+        >
+          Delete
+      </Button>
+        {/* This Button uses a Font Icon, see the installation instructions in the Icon component docs. */}
+        <Button
+          variant="contained"
+          color="primary"
+          endIcon={<Icon>send</Icon>}
+        >
+          Send
+      </Button>
+        <Button
+          variant="contained"
+          color="default"
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload
+      </Button>
+        <Button
+          variant="contained"
+          disabled
+          color="secondary"
+          startIcon={<KeyboardVoiceIcon />}
+        >
+          Talk
+      </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="small"
+          startIcon={<SaveIcon />}
+        >
+          Save
+      </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<SaveIcon />}
+        >
+          Save
+      </Button>
+      </div>
+      <br />
+      <br />
+      <br />
+      <IconButton aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton aria-label="delete" disabled color="primary">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton color="secondary" aria-label="add an alarm">
+        <AlarmIcon />
+      </IconButton>
+      <IconButton color="primary" aria-label="add to shopping cart">
+        <AddShoppingCartIcon />
+      </IconButton>
+      <br />
+      <br />
+      <br />
+      <Button variant="contained">
+        Default
+      </Button>
       <Button variant="contained" color="primary">
         Primary
       </Button>
@@ -64,13 +151,32 @@ const App = (props) => {
       <Button variant="outlined" color="primary" href="#outlined-buttons">
         Link
       </Button>
-
+      <br />
+      <br />
+      <br />
       <form>
         <TextField id="standard-basic" label="Standard" />
         <TextField id="filled-basic" label="Filled" variant="filled" />
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       </form>
 
+
+      <br />
+      <br />
+      <br />
+
+
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      </BottomNavigation>
     </ThemeProvider>
   );
 }
